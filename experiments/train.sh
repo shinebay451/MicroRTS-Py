414323cdef8076d843b8e3efe38767fce9bd9920
Colinster327 <cmurph0327@gmail.com>
2025-02-17 20:29:00 -0800

    fix: make train.sh executable

--- experiments/train.sh
+++ experiments/train.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cd "$HOME/Documents/repositories/MicroRTS-Py/experiments" || exit
-poetry shell
+cd "/home/cm787623/Documents/repositories/MicroRTS-Py/experiments" || exit
+source /home/cm787623/.cache/pypoetry/virtualenvs/gym-microrts-ZnOR3e21-py3.9/bin/activate
 
 python ppo_gridnet.py \
   --total-timesteps 100000000 \
@@ -12,4 +12,4 @@ python ppo_gridnet.py \
   --train-maps maps/16x16/basesWorkers16x16B.xml maps/16x16/basesWorkers16x16C.xml maps/16x16/basesWorkers16x16D.xml maps/16x16/basesWorkers16x16E.xml maps/16x16/basesWorkers16x16F.xml \
   --reward-weight 8.736320496 2.818786621 0.794951081 1.050905228 1.104272008 4.690114021
 
-exit
+deactivate
