cc7e676b49ea1f6102eedc57057668ff6ab86e48
Colin Murphy <cmurph0327@gmail.com>
2025-02-17 22:48:08 -0500

    add training script for disbatching

---
+++ experiments/train.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd "$HOME/Documents/repositories/MicroRTS-Py/experiments" || exit
+poetry shell
+
+python ppo_gridnet.py \
+  --total-timesteps 100000000 \
+  --seed 1 \
+  --prod-mode \
+  --wandb-project-name microrts-py \
+  --exp-name rewards-parametrization-27 \
+  --train-maps maps/16x16/basesWorkers16x16B.xml maps/16x16/basesWorkers16x16C.xml maps/16x16/basesWorkers16x16D.xml maps/16x16/basesWorkers16x16E.xml maps/16x16/basesWorkers16x16F.xml \
+  --reward-weight 8.736320496 2.818786621 0.794951081 1.050905228 1.104272008 4.690114021
+
+exit
