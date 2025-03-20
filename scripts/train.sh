582ecba141c5b8034b25c0d69162624327ba1fc6
Colinster327 <cmurph0327@gmail.com>
2025-03-20 15:04:07 -0700

    update mcrp sh script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 #SBATCH --gpus-per-node=1
-#SBATCH --time=2-00:00:00
+#SBATCH --time=7-00:00:00
 
-cd "/home/cm787623/Documents/repositories/MicroRTS-Py/experiments" || exit
-source /home/cm787623/.cache/pypoetry/virtualenvs/gym-microrts-ZnOR3e21-py3.9/bin/activate
+cd "$HOME/Documents/repositories/MicroRTS-Py" || exit
+source .venv/bin/activate
+cd ./experiments
 
 python ppo_gridnet.py \
-  --total-timesteps 100000000 \
+  --total-timesteps 200000000 \
   --seed 1 \
   --prod-mode \
   --wandb-project-name microrts-py \
   --exp-name rewards-parametrization-27 \
-  --train-maps maps/16x16/basesWorkers16x16B.xml maps/16x16/basesWorkers16x16C.xml maps/16x16/basesWorkers16x16D.xml maps/16x16/basesWorkers16x16E.xml maps/16x16/basesWorkers16x16F.xml \
   --reward-weight 8.736320496 2.818786621 0.794951081 1.050905228 1.104272008 4.690114021
-
-deactivate
